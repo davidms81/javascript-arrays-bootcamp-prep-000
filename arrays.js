7883a9324887cacff96a439ad286e6b4868f08b6
David Schoenholtz <dmschoenholtz@gmail.com>
2018-06-25 17:29:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(element, array) {
   return [element, ...array]
 }
+
+function addElementToBeginningOfArray(element, array) {
+  return [element, ...array]
+}
