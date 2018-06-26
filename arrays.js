9b6ed93f8ef37a03981bfedc45d864f4679181a3
David Schoenholtz <dmschoenholtz@gmail.com>
2018-06-26 14:30:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,6 @@ function addElementToBeginningOfArray(element, array) {
 }
 
 function destructivelyAddElementToEndOfArray(element, array) {
+  array.unshift(element)
   return [element, ...array]
 }
